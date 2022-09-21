655e636fc61f6a59cf4d0eb4241d82a43297ef2b
Praveen kumar <106659883+praveenkumarpinapothu@users.noreply.github.com>
2022-09-22 00:27:10 +0530

    import java.util.*; class celfh {      public static void main(String args[])      {           int c;           double f;           Scanner sc=new Scanner(System.in);           c=sc.nextInt();           f=((9*c)/(double)5)+32;           System.out.printf("%.2f",f);      } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class celfh
+{
+     public static void main(String args[])
+     {
+          int c;
+          double f;
+          Scanner sc=new Scanner(System.in);
+          c=sc.nextInt();
+          f=((9*c)/(double)5)+32;
+          System.out.printf("%.2f",f);
+     }
+}
