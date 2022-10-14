c22bb98f16df069940625007e26bb7da56c9fcb2
Praveen kumar <106659883+praveenkumarpinapothu@users.noreply.github.com>
2022-10-14 10:48:16 +0530

    import java.util.*; class Sample{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n,y;         n=sc.nextInt();         int[] x=new int[n];         for(int i=0;i<n;i++){             x[i]=sc.nextInt();         }         y=sc.nextInt();         int amt=0;         for (int j=0;j<n;j++){             if(x[j]>y){                 amt=amt+2;             }             else{                 amt=amt+1;             }         }         System.out.println(amt);     } }

---
+++ Airport__authority.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Sample{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n,y;
+        n=sc.nextInt();
+        int[] x=new int[n];
+        for(int i=0;i<n;i++){
+            x[i]=sc.nextInt();
+        }
+        y=sc.nextInt();
+        int amt=0;
+        for (int j=0;j<n;j++){
+            if(x[j]>y){
+                amt=amt+2;
+            }
+            else{
+                amt=amt+1;
+            }
+        }
+        System.out.println(amt);
+    }
+}
