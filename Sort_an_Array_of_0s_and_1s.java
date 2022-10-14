411d3a2bb8c135f9cbc586472c38a8ae3a321842
Praveen kumar <106659883+praveenkumarpinapothu@users.noreply.github.com>
2022-10-14 10:28:12 +0530

    import java.util.*; import java.util.ArrayList; import java.util.Arrays; class Sample{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         Arrays.sort(arr);         for(int i=0;i<n;i++){            System.out.print(arr[i]+" ");         }     } }

---
+++ Sort_an_Array_of_0s_and_1s.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+class Sample{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+           System.out.print(arr[i]+" "); 
+        }
+    }
+}
